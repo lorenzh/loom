09228012acd981bb62dd87e0697523c0bfdc34e2
hiloagent <hiloagent@users.noreply.github.com>
2026-04-01 00:28:42 +0200

    ✨ feat: add in_reply_to field and sendReply() to Message

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -4,5 +4,16 @@ export { loomHome } from "./env";
 export { InboxRouter, type InboxRouterOptions } from "./inbox-router";
 export { InboxWatcher, type InboxWatcherOptions } from "./inbox-watcher";
 export { AgentLogger, type LogEntry, type LogLevel } from "./logger";
-export { acknowledge, claim, consume, list, type Message, quarantine, read, send } from "./message";
+export {
+  acknowledge,
+  claim,
+  consume,
+  isMessage,
+  list,
+  type Message,
+  quarantine,
+  read,
+  send,
+  sendReply,
+} from "./message";
 export { ProcessTable } from "./process-table";
